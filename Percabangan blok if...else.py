6b3defcd48cb04e028e689c3e46c2b8a2c0091b5
Kaspul Asror <kaspul320@gmail.com>
2024-12-06 09:07:32 +0700

    Add files via upload

---
+++ Percabangan blok if...else.py
@@ -0,0 +1,13 @@
+nilai = 50
+
+print('Nilai anda adalah:', nilai, '\n')
+
+if nilai >= 70:
+  print('Selamat, anda lulus!')
+else:
+  print('Maaf, anda tidak lulus.')
+
+
+#Anda bisa mengganti nilai = 50 menjadi inputan user seperti ini.
+
+nilai = int(input('Masukkan nilai anda: '))
---
+++ Percabangan_IF.py
@@ -0,0 +1,6 @@
+if 5 > 10: # ❌
+  print('Nilai 5 lebih dari 10')
+
+if 10 > 5: # ✅
+  print('Nilai 10 lebih dari 5')
+print('Kode program ini akan selalu dieksekusi karena tidak termasuk pada percabangan')
